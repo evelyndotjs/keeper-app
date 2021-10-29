90e38c5590b5e05de95ed636ab3207981c269bb2
Evelyn Huang <evelyn@Evelyns-MacBook-Pro.local>
2021-10-29 14:52:29 +0200

    Pass props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
